dcfdd4f41843d1b5a83bc8291ad7fc0d11ead295
yaochenkun <690559724@qq.com>
2017-11-16 10:17:46 +0800

    修改为PropertyUtil

--- common/src/main/java/org/bupt/aiop/common/constant/CodeConsts.java
+++ common/src/main/java/org/bupt/aiop/common/constant/CodeConsts.java
@@ -8,4 +8,6 @@ public class CodeConsts {
 	public static final String SUCCESS = "SUCCESS";
 	public static final String FAILURE = "FAILURE";
 
+	public static final Integer CRUD_SUCCESS = 1;
+	public static final Integer CRUD_FAILURE = 0;
 }
