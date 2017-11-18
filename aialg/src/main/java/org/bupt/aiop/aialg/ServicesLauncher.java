184375d30f968f3a56952b6a2ff7f4eae9fa8047
yaochenkun <690559724@qq.com>
2017-11-18 14:02:31 +0800

    aialg普通spring工程Maven打包插件配置、mis热部署文件夹配置、将zookeeper地址统一换成实验室服务器

--- aialg/src/main/java/org/bupt/aiop/aialg/ServicesLauncher.java
+++ aialg/src/main/java/org/bupt/aiop/aialg/ServicesLauncher.java
@@ -2,21 +2,41 @@ package org.bupt.aiop.aialg;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 /**
  * Created by ken on 2017/9/23.
  */
 public class ServicesLauncher {
 
-	public static void main(String[] args) throws Exception {
+	private static Object lock = new Object();
+
+	public static void main(String[] args) throws InterruptedException {
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[]{"classpath:spring/dubbo-server.xml"});
+				new String[]{"classpath:spring/application-context.xml",
+						 	 "classpath:spring/dubbo-server.xml"});
 		context.start();
 
-		System.out.println("dubbo server has been successfully launched~");
+		System.out.println("dubbo server has been successfully launched on PID = " + getProcessID());
 
-		//程序挂起
-		while(true) System.in.read();
+		hangup();
+	}
+
+	public static void hangup() throws InterruptedException {
+		while(true) {
+			synchronized(lock) {
+				lock.wait();
+			}
+		}
+	}
+
+	public static final int getProcessID() {
+		RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+		System.out.println(runtimeMXBean.getName());
+		return Integer.valueOf(runtimeMXBean.getName().split("@")[0])
+				.intValue();
 	}
 
 }
