af63b4d2f2ccd4c535a5e856b698a8d6872f6ef3
yaochenkun <690559724@qq.com>
2017-11-18 22:33:18 +0800

    修改Thrift连接池工厂的valid/active/passive/destroy方法

--- common/src/main/java/org/bupt/common/thrift/ThriftConnectionFactory.java
+++ common/src/main/java/org/bupt/common/thrift/ThriftConnectionFactory.java
@@ -7,6 +7,7 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
 
 /**
  * Thrift RPC连接工厂
@@ -27,24 +28,46 @@ public class ThriftConnectionFactory implements PooledObjectFactory<TProtocol> {
 		return new DefaultPooledObject<TProtocol>(protocol);
 	}
 
+	@Override
 	public void destroyObject(PooledObject<TProtocol> pooledObject) throws Exception {
+		passivateObject(pooledObject);
+		pooledObject.markAbandoned();
+	}
+
+	@Override
+	public boolean validateObject(PooledObject<TProtocol> pooledObject) {
 
 		TProtocol protocol = pooledObject.getObject();
-		if (protocol.getTransport().isOpen()) {
-			protocol.getTransport().close();
+		if (protocol != null){
+			TTransport transport = protocol.getTransport();
+			if (transport.isOpen()){
+				return true;
+			}
+			try {
+				transport.open();
+				return  true;
+			} catch (TTransportException e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void activateObject(PooledObject<TProtocol> pooledObject) throws Exception {
+		TTransport transport = pooledObject.getObject().getTransport();
+		if (!transport.isOpen()){
+			transport.open();
 		}
 	}
 
-	public boolean validateObject(PooledObject<TProtocol> pooledObject) {
-		return pooledObject.getObject().getTransport().isOpen();
-	}
-
-	public void activateObject(PooledObject<TProtocol> pooledObject) throws Exception {
-
-	}
-
+	@Override
 	public void passivateObject(PooledObject<TProtocol> pooledObject) throws Exception {
-
+		TTransport transport = pooledObject.getObject().getTransport();
+		if (transport.isOpen()) {
+			transport.flush();
+			transport.close();
+		}
 	}
 
 
