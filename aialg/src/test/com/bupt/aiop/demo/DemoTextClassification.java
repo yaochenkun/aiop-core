4c41e740b4b764089d46795be43c3516b7813e94
liwangadd <liwangadd@gmail.com>
2017-12-13 19:05:36 +0800

    test

--- aialg/src/test/com/bupt/aiop/demo/DemoTextClassification.java
+++ aialg/src/test/com/bupt/aiop/demo/DemoTextClassification.java
@@ -2,32 +2,37 @@ package com.bupt.aiop.demo;
 
 import com.hankcs.hanlp.classification.classifiers.IClassifier;
 import com.hankcs.hanlp.classification.classifiers.NaiveBayesClassifier;
-import com.hankcs.hanlp.classification.models.NaiveBayesModel;
-import com.hankcs.hanlp.corpus.io.IOUtil;
 
-import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Map;
 
 public class DemoTextClassification {
 
-    public static final String CORPUS_FOLDER = "/home/windylee/Documents/Data/搜狗文本分类语料库迷你版";
+    public static final String CORPUS_FOLDER = "/home/windylee/Documents/Data/THUCTC/THUCNews/THUCNews";
+//
+//    static{
+//        File file = new File(CORPUS_FOLDER);
+//        if(!file.exists()||!file.isDirectory()){
+//            System.out.println("语料文件不存在");
+//            System.exit(1);
+//        }
+//    }
 
-    static{
-        File file = new File(CORPUS_FOLDER);
-        if(!file.exists()||!file.isDirectory()){
-            System.out.println("语料文件不存在");
-            System.exit(1);
+    public static void main(String[] args) {
+//        NaiveBayesModel model = (NaiveBayesModel) IOUtil.readObjectFrom("/home/windylee/Documents/classifier.model");
+        IClassifier classifier = new NaiveBayesClassifier();
+        try {
+            classifier.train(CORPUS_FOLDER);
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("/home/windylee/Documents/classifier.model"));
+            out.writeObject(classifier.getModel());
+        }catch (Exception e){
+            e.printStackTrace();
         }
-    }
 
-    public static void main(String[] args) throws IOException {
-        NaiveBayesModel model = (NaiveBayesModel) IOUtil.readObjectFrom("/home/windylee/Documents/classifier.model");
-        IClassifier classifier = new NaiveBayesClassifier(model);
-//        classifier.train(CORPUS_FOLDER);
-//        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("/home/windylee/Documents/classifier.model"));
-//        out.writeObject(classifier.getModel());
-
+        predict(classifier, "小米更新了新的系统，新系统支持免费拨打电话");
+        predict(classifier, "《清明上河图》是一幅文学价值极高的史诗画作");
         predict(classifier, "NBA-哈登31分火箭大胜");
         predict(classifier, "英国造航母耗时9年仍未服役 被中国速度圆圆摔在身后");
         predict(classifier, "研究生考录模式亟待进一步专业化");
--- mis/src/main/java/org/bupt/aiop/mis/mapper/UserMapper.java
+++ mis/src/main/java/org/bupt/aiop/mis/mapper/UserMapper.java
@@ -3,5 +3,5 @@ package org.bupt.aiop.mis.mapper;
 import org.bupt.aiop.mis.pojo.po.User;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface UserMapper extends Mapper<User> {
+public interface UserMapper extends Mapper<User> {z
 }
